1d534e87fef6da7058cb4f183eb1bc70a482865e
liorjacobian <lior.jacobian@gmail.com>
2022-04-04 17:27:45 +0300

    create play.go

---
+++ play.go
@@ -0,0 +1,13 @@
+package play
+
+func max(num1, num2 int) int {
+   /* local variable declaration */
+   result int
+
+   if (num1 > num2) {
+      result = num1
+   } else {
+      result = num2
+   }
+   return result 
+}
